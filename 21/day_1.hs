e7922975a668008fe4960fa0bf71c1cde785b348
KvanQuilst <eskew.dylan@outlook.com>
2021-12-02 07:24:05 -0800

    21: trying haskell sol

--- 21/day_1.hs
+++ 21/day_1.hs
@@ -6,12 +6,33 @@
 ----------------------
 import System.IO
 
+main :: IO ()
 main = do
-  
-  -- 1. Sonar Sweep
-  depth <- readFile "day1_1.in"
-  print timesDeeper depth -1
-  
+  input <- openFile "day1_1.hs" ReadMode
+  timesDeeper' input (-1) 
+  hClose input
 
-timesDeeper :: IO String -> Int -> Int
-timesDeeper input val1 =
+timesDeeper' :: Handle -> Int -> Int
+timesDeeper' _ 0 = 0
+timesDeeper' input y
+  | y == -1 = timesDeeper' input x
+  | otherwise = maxBin x y + timesDeeper' input x
+  where x = getInt input
+
+getInt :: Handle -> Int
+getInt input = do
+  hGetLine input
+  num <- read line
+  return num
+
+timesDeeper :: [Int] -> Int -> Int
+timesDeeper [] _ = error "list cannot be empty"
+timesDeeper [x] y = maxBin x y
+timesDeeper (x:xs) y
+  | y == -1 = timesDeeper xs x
+  | otherwise =  maxBin x y + timesDeeper xs x
+
+maxBin :: Int -> Int -> Int
+maxBin x y
+  | x >= y = 0
+  | y < x = 1
