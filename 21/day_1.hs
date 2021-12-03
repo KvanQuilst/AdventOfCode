9d74814a8331ec91e69461c6c87030c15d4ab2c8
KvanQuilst <eskew.dylan@outlook.com>
2021-12-03 09:55:53 -0800

    day 1: finsihed in haskell

--- 21/day_1.hs
+++ 21/day_1.hs
@@ -14,14 +14,25 @@ main = do
   let nums = map read $ lines contents
   putStr "The number of depth increases is "
   print $ timesDeeper nums (-1)
+  putStr "\n"
+
+  putStr "The number of the sum of depth increases is "
+  print $ timesDeeperAdv nums (-1)
 
 timesDeeper :: [Int] -> Int -> Int
---timesDeeper :: [IO a] -> a -> a
 timesDeeper [] _ = error "list cannot be empty"
 timesDeeper [x] y = maxBin x y
 timesDeeper (x:xs) y
   | y == -1 = timesDeeper xs x
   | otherwise =  maxBin x y + timesDeeper xs x
+
+timesDeeperAdv :: [Int] -> Int -> Int
+timesDeeperAdv [] _ = error "list cannot be empty"
+timesDeeperAdv (x:y:z:[]) val = maxBin (x+y+z) val
+timesDeeperAdv (x:y:z:xs) val
+  | val == -1 = timesDeeperAdv (y:z:xs) sum
+  | otherwise = maxBin sum val + timesDeeperAdv (y:z:xs) sum
+  where sum = x+y+z
 
 maxBin :: Int -> Int -> Int
 maxBin x y
