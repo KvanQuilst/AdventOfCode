7fe537731749ffbc1ddc711ed4af53d55c6c00f0
KvanQuilst <eskew.dylan@outlook.com>
2021-12-03 21:16:54 -0800

    21 day 4: perhaps some ideas

--- 21/day_4.hs
+++ 21/day_4.hs
@@ -10,3 +10,12 @@ import Data.List
 main :: IO ()
 main = do
   contents <- readFile "day4.in"
+
+  let input = lines contents
+  let calls = head input
+  let boards = tail input
+
+-- Notes:
+  -- Break up into individual boards --> transpose will be our friend  
+  -- Print the first successfull board
+  -- Pair number to bit whether checked?
