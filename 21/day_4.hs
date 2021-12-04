3cddd9607d091e4c9b7fc1f9e1538b06bc0ac36f
KvanQuilst <eskew.dylan@outlook.com>
2021-12-03 21:09:41 -0800

    21 day 4: added files

---
+++ 21/day_4.hs
@@ -0,0 +1,12 @@
+----------------------
+--
+-- Advent of Code - Day 1
+-- Binary Diagnostic
+--
+----------------------
+import System.IO
+import Data.List
+
+main :: IO ()
+main = do
+  contents <- readFile "day4.in"
