88dcae8a1db6d0234d29c9b53e61134351b591c4
Dylan E <6961140+KvanQuilst@users.noreply.github.com>
2023-12-08 11:21:31 -0800

    23: day8 init

---
+++ 23/day8/day8.py
@@ -0,0 +1,18 @@
+def part1():
+    print("Advent of Code 2023, Day 8.1")
+
+    with open("day8_test.in", 'r') as file:
+        lines = file.readlines()
+
+
+def part2():
+    print("Advent of Code 2023, Day 8.2")
+
+    with open("day8_test.in", 'r') as file:
+        lines = file.readlines()
+
+
+if __name__ == "__main__":
+    part1()
+    print()
+    part2()
