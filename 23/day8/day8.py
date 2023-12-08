0e1378576f91bd2a4787f3509cc384e70e28f931
Dylan E <6961140+KvanQuilst@users.noreply.github.com>
2023-12-08 13:43:26 -0800

    23: day8, part 2 very slow...

--- 23/day8/day8.py
+++ 23/day8/day8.py
@@ -18,8 +18,10 @@ def part1():
 
         while curr != 'ZZZ':
             if dirs[dir_idx] == 'L':
+                #print(curr, "->", network[curr][0], dir_idx)
                 curr = network[curr][0]
             else: # R
+                #print(curr, "->", network[curr][1], dir_idx)
                 curr = network[curr][1]
             dir_idx = (dir_idx + 1) % len(dirs)
             steps += 1
@@ -29,8 +31,29 @@ def part1():
 def part2():
     print("Advent of Code 2023, Day 8.2")
 
-    with open("day8_test.in", 'r') as file:
+    steps = 0
+    with open("day8.in", 'r') as file:
         lines = file.readlines()
+
+        dirs = lines[0].strip()
+
+        network = [re.split(' = \(|, |\)', line[:-2]) for line in lines[2:]]
+        network = map(lambda x: (x[0], (x[1], x[2])), network)
+        network = dict(network)
+
+        curr = [x for x in network.keys() if x[2] == 'A']
+        dir_idx = 0
+
+        while not all(x[2] == 'Z' for x in curr):
+            if dirs[dir_idx] == 'L':
+                curr = [network[x][0] for x in curr]
+            else:
+                curr = [network[x][1] for x in curr]
+
+            dir_idx = (dir_idx + 1) % len(dirs)
+            steps += 1
+
+    print("The number of steps is", steps)
 
 
 if __name__ == "__main__":
