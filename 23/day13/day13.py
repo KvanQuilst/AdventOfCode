f01ac2b9dda60e46b2e5bdf6e0281e4f25b1eced
Dylan E <6961140+KvanQuilst@users.noreply.github.com>
2023-12-13 14:32:48 -0800

    23: day 13 finished

--- 23/day13/day13.py
+++ 23/day13/day13.py
@@ -3,20 +3,84 @@ def h_symmetry(pattern):
     for i, l in enumerate(pattern[1:], 1):
         sym = True
         for j in range(0, i):
-            if i + j >= len(pattern) or i-j-1 <= 0:
+            if i + j >= len(pattern) or i-j-1 < 0:
                 break
 
-            if not pattern[i+j] == pattern[i-j-1]:
+            if pattern[i+j] != pattern[i-j-1]:
                 sym = False
+                break
 
         if sym:
-            sum += 100
+            sum += 100 * i
 
     return sum
 
 
 def v_symmetry(pattern):
     sum = 0
+    for i, l in enumerate(pattern[0][1:-1], 1):
+        sym = True
+        for j in range(0, i):
+            if i + j >= len(pattern[0])-1 or i-j-1 < 0:
+                break
+
+            if [x[i+j] for x in pattern] != [x[i-j-1] for x in pattern]:
+                sym = False
+                break
+            
+        if sym:
+            sum += i
+
+    return sum
+
+
+def h_sym_smudge(pattern):
+    sum = 0
+    for i, l in enumerate(pattern[1:], 1):
+        sym = True
+        diff_1 = False
+        for j in range(0, i):
+            if i + j >= len(pattern) or i-j-1 < 0:
+                break
+
+            p1 = pattern[i+j]
+            p2 = pattern[i-j-1]
+            diff = len([1 for x in zip(p1, p2) if x[0] != x[1]])
+            if diff == 1:
+                diff_1 = True
+
+            if p1 != p2 and diff > 1:
+                sym = False
+                break
+
+        if sym and diff_1:
+            sum = 100 * i
+
+    return sum
+
+
+def v_sym_smudge(pattern):
+    sum = 0
+    for i, l in enumerate(pattern[0][1:-1], 1):
+        sym = True
+        diff_1 = False
+        for j in range(0, i):
+            if i + j >= len(pattern[0])-1 or i-j-1 < 0:
+                break
+
+            p1 = [x[i+j] for x in pattern]
+            p2 = [x[i-j-1] for x in pattern]
+            diff = len([1 for x in zip(p1, p2) if x[0] != x[1]])
+            if diff == 1:
+                diff_1 = True
+
+            if p1 != p2 and diff > 1:
+                sym = False
+                break
+            
+        if sym and diff_1:
+            sum = i
+
     return sum
 
 
@@ -24,7 +88,7 @@ def part1():
     print("Advent of Code 2023: Day 13.1")
 
     sum = 0
-    with open("day13_test.in", 'r') as file:
+    with open("day13.in", 'r') as file:
         lines = file.readlines()
 
         # Horizontal
@@ -45,8 +109,23 @@ def part1():
 def part2():
     print("Advent of Code 2023: Day 13.2")
 
-    with open("day13_test.in", 'r') as file:
+    sum = 0
+    with open("day13.in", 'r') as file:
         lines = file.readlines()
+
+        # Horizontal
+        pattern = []
+        for line in lines:
+            if not line.isspace():
+                pattern.append(line)
+
+            else:
+                sum += h_sym_smudge(pattern) + v_sym_smudge(pattern)
+                pattern = []
+
+        sum += h_sym_smudge(pattern) + v_sym_smudge(pattern)
+
+        print("The sum is", sum)
 
 
 if __name__ == "__main__":
